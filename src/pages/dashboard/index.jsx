13f2f2b6ae036ba141b69223558a9e65e687f43f
echo <heart0magic@163.com>
2023-03-01 11:41:30 +0800

    工龄最久的员工图表组件编写

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { useSelector } from 'umi';
 import './css/index.less'
 import StaffAmount from './components/StaffAmount';
+import OldStaffTable from './components/OldStaffTable';
 
 const Dashborad = () => {
-    const {amountList} = useSelector(state => state.dashboard);
+    const {amountList,oldStaffData } = useSelector(state => state.dashboard);
     console.log(amountList)
     return (
         <div className='dashboard-container'>
             {
                 amountList.map( (props,index) => <StaffAmount key={index} {...props} />)
             }
+
+            <OldStaffTable {...oldStaffData} />
         </div>
     )
 }
