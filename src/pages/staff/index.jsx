7483f007e4b4b3189b514227a7ff9ba15f148368
echo <heart0magic@163.com>
2023-03-03 13:15:11 +0800

    员工主页面基本结构搭建

--- src/pages/staff/index.jsx
+++ src/pages/staff/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect , useState } from 'react';
+import { useDispatch } from 'umi';
+import './index.less';
 
-const staff = () => {
-  return (
-    <div>index</div>
-  )
+const staff = ({ }) => {
+    const [page, setPage] = useState(1)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        _initStaffList();
+    }, [])
+
+    const _initStaffList = () => dispatch({ type: 'staff/initStaffList', payload: { size: 10, page } })
+
+    return (
+        <div className='staff-container'>
+
+        </div>
+    )
 }
 
 export default staff
