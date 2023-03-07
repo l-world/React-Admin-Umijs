81210d663b40a0dee2d1b1460c6fb2ff82c79a0a
echo <heart0magic@163.com>
2023-03-07 16:17:41 +0800

    修复分页组件默认值为第一页，而不是第0页

--- src/components/TableHeader/index.jsx
+++ src/components/TableHeader/index.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './index.less'
-import { Button, Pagination} from 'antd';
+import { Button, Pagination } from 'antd';
 import IconMap from '../IconMap';
 import classNames from 'classNames';
 import { useSelector } from 'umi';
 
 const TableHeader = ({ page, size, total, changePage, interfaceDelMethod }) => {
-   
-    const { collapse } = useSelector( state => state.common);
+
+    const { collapse } = useSelector(state => state.common);
     return (
-        <div className={ classNames('table-header-container',{'big-style': collapse }) } >
+        <div className={classNames('table-header-container', { 'big-style': collapse })} >
             <div>
                 <Button className='mr-10' size='small' shape='round' icon={IconMap.add} >创建</Button>
                 <Button danger size='small' shape='round' icon={IconMap.del}>批量删除</Button>
             </div>
             <div className="pagination-container">
-                <Pagination 
+                <Pagination
                     simple
                     defaultCurrent={page}
-                    currentPage={page}
+                    current={page}
                     pageSize={size}
                     total={total}
-                    onChange={ (page) => changePage(page)}
+                    onChange={(page) => changePage(page)}
                 />
-                <span >共计{ total }条记录</span>
+                <span >共计{total}条记录</span>
             </div>
         </div>
     )
