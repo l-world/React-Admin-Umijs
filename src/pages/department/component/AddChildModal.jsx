045c5f733754065c544b75c5be68e02561a5edf9
echo <heart0magic@163.com>
2023-03-17 16:17:41 +0800

    解除子部门功能实现

--- src/pages/department/component/AddChildModal.jsx
+++ src/pages/department/component/AddChildModal.jsx
@@ -3,30 +3,38 @@ import { Modal, Table } from 'antd';
 import { useSelector } from 'umi';
 const { Column } = Table;
 
-const AddChildModal = ({ showChildModal, setShowChildModal, pushOrUpdateList}) => {
-
+const AddChildModal = ({
+    showChildModal,
+    setShowChildModal,
+    pushOrUpdateList,
+    existsList = [],
+}) => {
     const [childList, setChildList] = useState([]);
     const { departmentDetail } = useSelector((state) => state.department);
     const departmentList = useSelector((state) => {
-        return state.department.departmentList.filter((item) => item.parentLists[0] !== null);
+        return state.department.departmentList.filter((item) => {
+            return (
+                item.parentLists[0] !== null &&
+                departmentDetail?._id !== item._id &&
+                !existsList.map((item) => item._id).includes(item._id)
+            );
+        });
     });
 
-    // 清空 + 关闭弹窗
+    //- 清空+ 关闭弹窗的操作
     const clearSelect = () => {
-        setShowChildModal(false)
-    }
-
-    const addChildList = () => {
-        const sendData = { list: childList, type: departmentDetail ? "update" : "add" };
-        pushOrUpdateList(sendData);
         setShowChildModal(false);
-        // if (departmentDetail) {
-        //     pushOrUpdateList({ list: childList, type: 'update' });
-        //   } else {
-        //     pushOrUpdateList({ list: childList.concat(existsList), type: 'add' });
-        //   }
-        //   setShowChildModal(false);
-    }
+    };
+
+    //- 新增子部门操作
+    const addChildList = () => {
+        if (departmentDetail) {
+            pushOrUpdateList({ list: childList, type: 'update' });
+        } else {
+            pushOrUpdateList({ list: childList.concat(existsList), type: 'add' });
+        }
+        setShowChildModal(false);
+    };
 
     return (
         <Modal
@@ -34,19 +42,19 @@ const AddChildModal = ({ showChildModal, setShowChildModal, pushOrUpdateList}) =
             open={showChildModal}
             onCancel={clearSelect}
             onOk={addChildList}
-            destroyClose
+            destroyOnClose
         >
             <Table
-                dataSource={ departmentList }
+                dataSource={departmentList}
                 rowSelection={{ onChange: (ids, record) => setChildList(record) }}
                 pagination={false}
                 expandIconColumnIndex={-1}
-                rowKey={(record) =>  record?._id }
+                rowKey={(record) => record?._id}
             >
                 <Column title="部门名称" dataIndex="departmentName" />
             </Table>
         </Modal>
-    )
-}
+    );
+};
 
-export default AddChildModal
+export default AddChildModal;
--- src/pages/department/component/ChildDepartment.jsx
+++ src/pages/department/component/ChildDepartment.jsx
@@ -1,42 +1,59 @@
 import React, { useState } from 'react';
 import { Table, Button, Modal } from 'antd';
-import IconMap from 'components/IconMap';
+import iconMap from 'components/IconMap';
 import AddChildModal from './AddChildModal';
 
 const { Column } = Table;
-
-const ChildDepartment = ({childList,pushOrUpdateList}) => {
-
-    const [delIds, setDelIds] = useState([]);
+const childDepartment = ({ childList, pushOrUpdateList, departmentDetail }) => {
+    const [delList, setDelList] = useState([]);
     const [showDelModal, setShowDelModal] = useState(false);
     const [showChildModal, setShowChildModal] = useState(false);
 
-    // 增加子部门事件处理
-    const getDepartmentList =() => {
+    //- 增加子部门事件处理
+    const getDepartmentList = () => {
         setShowChildModal(true);
-    }
+    };
 
-    // 删除子部门
+    //- 删除子部门
     const delDepartment = () => {
-        console.log('del')
-    }
+        setShowDelModal(false);
+        if (departmentDetail) {
+            pushOrUpdateList({ list: delList, type: 'del' });
+        } else {
+            const ids = delList.map((item) => item._id);
+            const tempArr = childList.filter((item) => !ids.includes(item._id));
+            pushOrUpdateList({ list: tempArr, type: 'add' });
+        }
+    };
 
     return (
         <>
             <Table
                 dataSource={childList}
-                rowSelecttion={ { onchange:(ids) => setDelIds(ids) } }
+                rowSelection={{ onChange: (ids, record) => setDelList(record) }}
                 pagination={false}
-                expandIconColumnIndex={1}
-                rowKey={ (record) => record?._id }
+                expandIconColumnIndex={-1}
+                rowKey={(record) => record?._id}
             >
                 <Column title="名称" dataIndex="departmentName" />
             </Table>
-
-            {/* 操纵按钮 */}
+            {/* 操作按钮 */}
             <div className="operation">
-                <Button type="primary" style={{marginRight:'10px'}} icon={IconMap.api}  onClick={ getDepartmentList } >增加子部门</Button>
-                <Button  disabled={!delIds.length} icon={IconMap.del}  onClick={() => setShowDelModal(true)} >接触子部门关联</Button>
+                <Button
+                    type="primary"
+                    onClick={getDepartmentList}
+                    style={{ marginRight: '10px' }}
+                    icon={iconMap.api}
+                >
+                    增加子部门
+                </Button>
+                <Button
+                    onClick={() => setShowDelModal(true)}
+                    disabled={!delList.length}
+                    icon={iconMap.del}
+                >
+                    解除子部门关联
+                </Button>
             </div>
 
             {/* 新增子部门弹窗 */}
@@ -44,19 +61,20 @@ const ChildDepartment = ({childList,pushOrUpdateList}) => {
                 showChildModal={showChildModal}
                 setShowChildModal={setShowChildModal}
                 pushOrUpdateList={pushOrUpdateList}
+                existsList={childList}
             />
 
-            {/* 删除子部门弹窗 */}
+            {/* 删除子部门的弹窗 */}
             <Modal
                 title="提示"
-                open={ showDelModal }
-                onOk={ () => delDepartment  }
-                onCancel={ () => setShowDelModal(false) }
+                open={showDelModal}
+                onOk={delDepartment}
+                onCancel={() => setShowDelModal(false)}
             >
-                确定要删除选择的部门吗？
+                确定要删除选择的部门么?
             </Modal>
         </>
-    )
-}
+    );
+};
 
-export default ChildDepartment
+export default childDepartment;
